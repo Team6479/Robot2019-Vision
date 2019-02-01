72cdc3b592efbf07a2db3daa91b2f3523e42e2e3
Osnott <aidenonstott@gmail.com>
2019-02-01 10:15:22 -0700

    Started porting udp server code

--- frc2019_vision/networking.py
+++ frc2019_vision/networking.py
@@ -6,6 +6,10 @@ import netifaces
 from . import StoppableThread
 from .events import handler as event_handler
 
+import pickle
+
+import cv2
+
 
 class ConnectionType(Enum):
     TCP = 0
@@ -43,6 +47,39 @@ class RioConnectionFactoryThread(StoppableThread):
         self._server = socketserver.TCPServer(
             (self._ip, self._port), RioConnectionHandler
         )
+
+    def run(self):
+        try:
+            self._server.serve_forever()
+        except Exception:
+            self.stop()
+
+    def stop(self):
+        self._server.shutdown()
+
+
+class DriverstationConnectionHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        # initial = self.request[0].strip()
+        socket = self.request[1]
+        packets = 0
+        while True:
+            # print("CONNECTED: " + str(initial.decode('utf-8')))
+            # somehow get feed
+            feed = "placeholder"
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 40]
+            result, encimg = cv2.imencode('.jpg', feed, encode_param)
+            packet = pickle.dumps([encimg, packets])
+            socket.sendto(packet, self.client_address)
+            packets = packets + 1
+
+
+class DriverstationConnectionFactoryThread(StoppableThread):
+    def __init__(self):
+        self._HOST,
+        self._PORT = netifaces.ifadresses("eth0")[netifaces.AF_INET][0]["addr"], 9999
+        self._server = socketserver.UDPServer((self._HOST, self._PORT),
+                                              DriverstationConnectionHandler())
 
     def run(self):
         try:
