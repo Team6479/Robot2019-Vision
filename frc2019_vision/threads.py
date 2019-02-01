2fa217ee5b79953c280d2ffa6383a5bcd121b262
iboyperson <tjquillan@gmail.com>
2019-02-01 15:43:15 -0700

    actually start UDPthread

--- frc2019_vision/threads.py
+++ frc2019_vision/threads.py
@@ -1,16 +1,22 @@
-from .networking import RioConnectionFactoryThread
+from .networking import (
+    DriverstationConnectionFactoryThread,
+    RioConnectionFactoryThread,
+)
 from .vision.vision import VisionThread
 
 
 VISION_THREAD = VisionThread()
 RIO_THREAD = RioConnectionFactoryThread()
+DRIVERSTATION_THREAD = DriverstationConnectionFactoryThread()
 
 
 def start():
     VISION_THREAD.start()
     RIO_THREAD.start()
+    DRIVERSTATION_THREAD.start()
 
 
 def stop():
     VISION_THREAD.stop()
     RIO_THREAD.stop()
+    DRIVERSTATION_THREAD.stop()
